f32536b4c48a55c448a2f1b2b26fbe8dca795590
AlSerPan <aleksandor1203@yandex.ru>
2023-05-05 00:28:08 +0500

    Должно все работать

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,97 @@
+
+import java.util.Scanner
+
+class Archive(val name: String, val notes: MutableList<Note>)
+
+class Note(val title: String, val content: String)
+
+
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val app = NotesApp()
+    val scanner = Scanner(System.`in`)
+
+
+    while (true) {
+
+        println("Список архивов:")
+        val archives = app.getArchives()
+        for (i in archives.indices) {
+            println("$i. ${archives[i]}")
+        }
+        println("${archives.size}. Создать архив")
+        println("${archives.size + 1}. Выход")
+
+
+        if (scanner.hasNextInt()) {
+            val choice = scanner.nextInt()
+
+
+            when {
+                choice == archives.size -> {
+                    println("Введите название архива:")
+                    var name = scanner.next()
+                    app.addArchive(name)
+
+
+                }
+                choice == archives.size + 1 -> {
+
+                    break
+                }
+                choice < archives.size -> {
+                    val archiveName = archives[choice]
+                    while (true) {
+                        println("Список заметок в архиве '$archiveName':")
+                        val notes = app.getNotesFromArchive(choice)
+                        for (i in notes.indices) {
+                            println("$i. ${notes[i]}")
+                        }
+                        println("${notes.size}. Создать заметку")
+                        println("${notes.size + 1}. Назад")
+                        if (scanner.hasNextInt()) {
+                            val  noteChoice = scanner.nextInt()
+                            when {
+                                noteChoice == notes.size -> {
+                                    println("Введите название заметки:")
+                                    val title = scanner.next()
+                                    println("Введите содержание заметки:")
+                                    val content = readLine()
+                                    if (content != null) {
+                                        app.addNoteToArchive(choice, title, content)
+                                    }
+                                }
+                                noteChoice == notes.size + 1 -> {
+
+                                    break
+                                }
+                                noteChoice < notes.size -> {
+                                    val noteTitle = notes[noteChoice]
+                                    val noteContent =
+                                        app.getNoteContentFromArchive(choice, noteChoice)
+                                    println("Заметка '$noteTitle': $noteContent")
+                                }
+                                else -> {
+                                    println("Неверный выбор")
+                                }
+                            }
+                        }
+
+                        else {println("Введите число!")
+                            scanner.next() }
+
+                    }
+                }
+                else -> {
+                    println("Неверный выбор")
+                }
+
+            }
+
+        }
+
+        else {println("Введите число!")
+            scanner.next() }
+
+    }
 }
